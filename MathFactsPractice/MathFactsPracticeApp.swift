68668b222badd688ff7a68dce136c623fbef8a44
Joseph Jung <joseph.jung@icloud.com>
2022-04-13 13:26:42 -0600

    Initial Commit

---
+++ MathFactsPractice/MathFactsPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MathFactsPracticeApp.swift
+//  MathFactsPractice
+//
+//  Created by Joseph Jung on 4/13/22.
+//
+
+import SwiftUI
+
+@main
+struct MathFactsPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
